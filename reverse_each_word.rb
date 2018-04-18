19c3a69e2e69e769e3494e2d6dcfaf98a8491d5c
Michael Blandford <mdblandford@gmail.com>
2018-04-18 13:35:47 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
 def reverse_each_word(string)
+      # string = "Hello there, and how are you?"
   string = string.split(" ")    
       # created newArray = ["Hello", "there,","and","how","are", "you?"]
   newArray = string.collect{|eachWord| eachWord.reverse}     

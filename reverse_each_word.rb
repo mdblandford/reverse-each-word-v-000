d3c19ad924d56d0952b66c041f7fae6811b3d0ed
Michael Blandford <mdblandford@gmail.com>
2018-06-19 20:52:26 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,8 @@ def reverse_each_word(string)
       # string = "Hello there, and how are you?"
   #string = string.split(" ")
       # created newArray = ["Hello", "there,","and","how","are", "you?"]
-  newArray = string.collect{|eachWord| eachWord.reverse}
+  #newArray = 
+  string.collect{|eachWord| eachWord.reverse}
       # created newArray = ["olleH",",ereht","dna","woh","era","?uoy"]
   #newArray.join(" ")
 end
